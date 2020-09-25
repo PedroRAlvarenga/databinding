aababd296c811e1e65a3fa7045db92d8348c2286
PedroRAlvarenga <pedralvarenga@gmail.com>
2020-09-24 21:41:39 -0300

    Fim pdf 05-02

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'databinding';
+  /*nome = "Pedro"
+  idade = 19
+
+  lancarDado (): number {
+    return Math.floor(Math.random() * 6) + 1
+  }
+*/
+
+nome: string;
+exibeCaixa: boolean = false;
+
+alterarNome (nome):void{
+  console.log(nome.target.value);
+  this.nome=nome.target.value;
 }
+  adicionar (nomeInput): void {
+    console.log (nomeInput.value);
+    this.nome= nomeInput.value;
+    this.exibeCaixa = true;
+    //console.log("Adicionando...")
+  }
+}
+
